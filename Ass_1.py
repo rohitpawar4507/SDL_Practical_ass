8f62762ced8859d047958c5670ea0a397120d157
rohitpawar4507 <67743293+rohitpawar4507@users.noreply.github.com>
2020-09-28 22:13:55 +0530

    Add files via upload

---
+++ Ass_1.py
@@ -0,0 +1,17 @@
+
+# SDL- Assignment NO.1
+
+text = open("sdl.txt", "r")
+d=dict()
+
+for line in text:
+    line=line.lower()
+    words=line.split(" ")
+
+    for word in words:
+        if word in d:
+            d[word]=d[word]+1
+        else:
+            d[word]=1
+for key in list(d.keys()):
+    print(key,":",d[key])
