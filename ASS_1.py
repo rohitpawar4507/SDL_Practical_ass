230f7b788da63d9eaf5aff4166f9d1ba7521ad1d
rohitpawar4507 <67743293+rohitpawar4507@users.noreply.github.com>
2020-09-30 17:12:23 +0530

    Create ASS_1.py

---
+++ ASS_1.py
@@ -0,0 +1,19 @@
+fo=open("sdl.txt",'r')
+str=fo.read()
+data=str.split()
+counts=dict()
+
+for i in data:
+    if i in counts:
+        counts[i]+=1
+    else:
+            counts[i]=1
+
+w=len(str)
+l=len(data)
+
+print("Total Characters in the file is :",w)
+print("Total Words in the file is :",l)
+print("occurrence:",counts)
+
+fo.close()
